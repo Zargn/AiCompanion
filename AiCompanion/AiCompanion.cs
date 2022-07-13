7bc945e95a4dd610667f65c7f8b0b43496e0a626
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-13 21:42:05 +0200

    Initial preparation commit.

---
+++ AiCompanion/AiCompanion.cs
@@ -0,0 +1,6 @@
+﻿namespace AiCompanion;
+
+public class AiCompanion
+{
+        
+}
---
+++ LocalUi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalUi;
+using AiCompanion;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var aiCompanion = new AiCompanion();
+    }
+}
