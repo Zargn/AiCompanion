84224ae8aee97bde3b32b6a8d105b192d99282a3
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-15 19:20:47 +0200

    BrainLoop.cs: Added base thought loop

--- AiCompanion/Logic/BrainLoop.cs
+++ AiCompanion/Logic/BrainLoop.cs
@@ -6,7 +6,25 @@ internal class BrainLoop
 {
     private Queue<IThought> thoughtQueue = new();
 
+    internal bool Enabled;
+
     internal void AddThought(IThought thought)
+    {
+        
+    }
+
+    private void Loop()
+    {
+        while (Enabled)
+        {
+            if (thoughtQueue.Count == 0)
+                Idle();
+            else
+                thoughtQueue.Dequeue().Think();
+        }
+    }
+
+    private void Idle()
     {
         
     }
