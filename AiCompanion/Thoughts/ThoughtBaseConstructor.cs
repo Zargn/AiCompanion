51aa5b7da196015ba5e9dde396ad1a5a83d6a57e
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-16 19:35:57 +0200

    Create ThoughtBaseConstructor.cs
    Basically just a handy abstract class as a shitty but easy way to make sure I have the dependencies I need.

---
+++ AiCompanion/Thoughts/ThoughtBaseConstructor.cs
@@ -0,0 +1,11 @@
+﻿namespace AiCompanion.Thoughts;
+
+internal abstract class ThoughtBaseConstructor
+{
+    protected AiCompanion aiCompanion;
+    
+    internal ThoughtBaseConstructor(AiCompanion aiCompanion)
+    {
+        this.aiCompanion = aiCompanion;
+    }
+}
