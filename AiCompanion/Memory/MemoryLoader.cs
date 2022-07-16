6fa96320ef8fb9c560080662348f378aaac48ba9
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-16 19:37:39 +0200

    Created static MemoryLoader class.
    Will in future handle loading MemoryBanks from file. (Right now it just returns a new MemoryBank though.)

---
+++ AiCompanion/Memory/MemoryLoader.cs
@@ -0,0 +1,12 @@
+﻿namespace AiCompanion.Memory;
+
+internal static class MemoryLoader
+{
+    internal static MemoryBank LoadFromFile(string filepath)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("WARNING: MemoryLoader.LoadFromFile(string) Is not implemented! Currently it returns a new MemoryBank instead of loading old ones.");
+        Console.ResetColor();
+        return new MemoryBank();
+    }
+}
