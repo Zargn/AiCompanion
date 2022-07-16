c4bf47a4d7fe9dc99d118ec82d3bda8b460ede81
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-16 19:36:37 +0200

    Create Hear thought.
    Currently doesn't do much more than log some things.

---
+++ AiCompanion/Thoughts/Hear.cs
@@ -0,0 +1,19 @@
+﻿using AiCompanion.Interfaces;
+
+namespace AiCompanion.Thoughts;
+
+internal class Hear : ThoughtBaseConstructor, IThought
+{
+    public Hear(AiCompanion aiCompanion, string message) : base(aiCompanion)
+    {
+        aiCompanion.SendLog($"Created hear thought for: [{message}]");
+        this.message = message;
+    }
+
+    private string message;
+
+    public void Think()
+    {
+        aiCompanion.SendLog($"Companion heard [{message}]");
+    }
+}
