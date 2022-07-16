97f2cf08a65e60a65b8cee66f7b91a6d26a97962
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-16 18:24:11 +0200

    LocalUi.cs: Added base functionality
    Automatically configures companion output to be in console, and creates a loop of user input.

---
+++ LocalUi/LocalUi.cs
@@ -0,0 +1,32 @@
+﻿namespace LocalUi;
+using AiCompanion;
+
+public static class LocalUi
+{
+    public static void Main()
+    {
+        var aiCompanion = new AiCompanion();
+
+        ConfigureOutput(aiCompanion);
+        
+        RunUiLoop(aiCompanion);
+        
+        Console.WriteLine("Done");
+    }
+
+    private static void ConfigureOutput(AiCompanion aiCompanion)
+    {
+        aiCompanion.CompanionOutput += Console.WriteLine;
+    }
+
+    private static void RunUiLoop(AiCompanion aiCompanion)
+    {
+        while (true)
+        {
+            var userInput = Console.ReadLine();
+            if (userInput == null)
+                return;
+            aiCompanion.CompanionInput(userInput);
+        }
+    }
+}
