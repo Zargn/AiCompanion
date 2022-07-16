15a927ef2ada5f837c5423281c97e7b770c2085e
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-16 19:39:25 +0200

    Improved BrainLoop.cs to working state.

--- AiCompanion/Logic/BrainLoop.cs
+++ AiCompanion/Logic/BrainLoop.cs
@@ -6,16 +6,27 @@ internal class BrainLoop
 {
     private Queue<IThought> thoughtQueue = new();
 
-    internal bool Enabled;
+    private bool enabled = true;
 
+    private EventWaitHandle thoughtQueueEmpty = new AutoResetEvent(false);
+
+    private AiCompanion aiCompanion;
+    
+    public BrainLoop(AiCompanion aiCompanion)
+    {
+        this.aiCompanion = aiCompanion;
+        new Thread(Loop).Start();
+    }
+    
     internal void AddThought(IThought thought)
     {
-        
+        thoughtQueue.Enqueue(thought);
+        thoughtQueueEmpty.Set();
     }
 
     private void Loop()
     {
-        while (Enabled)
+        while (enabled)
         {
             if (thoughtQueue.Count == 0)
                 Idle();
@@ -26,6 +37,8 @@ internal class BrainLoop
 
     private void Idle()
     {
-        
+        aiCompanion.SendLog("Pause brain loop.");
+        thoughtQueueEmpty.WaitOne();
+        aiCompanion.SendLog("Resume brain loop.");
     }
 }
