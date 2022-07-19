b4f607805e2b6d80cc853ccb68171d10a69f52ed
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-19 14:27:12 +0200

    Created memory class with some functionality.

---
+++ AiCompanion/Memory/Memory.cs
@@ -0,0 +1,23 @@
+﻿namespace AiCompanion.Memory;
+
+internal class Memory
+{
+    internal string Title;
+    internal List<Memory> ChildMemories;
+    internal List<Memory> IsMemories;
+    internal List<Memory> HasMemories;
+
+    internal Memory(string title)
+    {
+        Title = title;
+    }
+
+    internal List<Memory> GetAllChildMemories()
+    {
+        var result = new List<Memory>();
+        foreach (var memory in ChildMemories)
+        {
+            result.AddRange(memory.ChildMemories);
+        }
+    }
+}
