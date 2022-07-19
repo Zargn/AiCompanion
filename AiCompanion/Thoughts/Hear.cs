4a1d8e71513486fdb92f820ac13d9cee83ac67ac
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-19 18:58:39 +0200

    Hear.cs: Creates analyzeWord thoughs for sentence

--- AiCompanion/Thoughts/Hear.cs
+++ AiCompanion/Thoughts/Hear.cs
@@ -15,5 +15,27 @@ internal class Hear : ThoughtBaseConstructor, IThought
     public void Think()
     {
         aiCompanion.SendLog($"Companion heard [{message}]");
+        var thoughts = new List<IThought>();
+        foreach (var word in ExtractWords(message))
+        {
+            thoughts.Add(new AnalyzeWord(aiCompanion, word));
+        }
+        aiCompanion.brainLoop.AddThoughts(thoughts);
+    }
+
+    private IEnumerable<string> ExtractWords(string sentence)
+    {
+        int nextWordStartPosition = 0;
+        int index;
+        for (index = 0; index < sentence.Length; index++)
+        {
+            if (sentence[index] is ' ' or '.' or ',')
+            {
+                yield return sentence.Substring(nextWordStartPosition, index - nextWordStartPosition);
+                nextWordStartPosition = index + 1;
+            }
+        }
+        
+        yield return sentence.Substring(nextWordStartPosition, index - nextWordStartPosition);
     }
 }
