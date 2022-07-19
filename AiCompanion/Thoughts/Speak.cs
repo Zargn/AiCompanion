53fb342c93ae4a8a1234d4f7ad414654e2f159a3
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-19 17:11:52 +0200

    Created Speak.cs thought for the ai to speak.

---
+++ AiCompanion/Thoughts/Speak.cs
@@ -0,0 +1,18 @@
+﻿using AiCompanion.Interfaces;
+
+namespace AiCompanion.Thoughts;
+
+internal class Speak : ThoughtBaseConstructor, IThought
+{
+    public Speak(AiCompanion aiCompanion, string message) : base(aiCompanion)
+    {
+        this.message = message;
+    }
+
+    private string message;
+    
+    public void Think()
+    {
+        aiCompanion.SendOutput(message);
+    }
+}
