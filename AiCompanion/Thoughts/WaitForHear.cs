54b39554427b470773ae9ea2ba20923218760d23
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-21 14:22:24 +0200

    Started work on a WaitForHear class

---
+++ AiCompanion/Thoughts/WaitForHear.cs
@@ -0,0 +1,15 @@
+﻿using AiCompanion.Interfaces;
+
+namespace AiCompanion.Thoughts;
+
+internal class WaitForHear : ThoughtBaseConstructor, IThought
+{
+    public WaitForHear(AiCompanion aiCompanion, string wordToWaitFor) : base(aiCompanion)
+    {
+    }
+
+    public void Think()
+    {
+        
+    }
+}
