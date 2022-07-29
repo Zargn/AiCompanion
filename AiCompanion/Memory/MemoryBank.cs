68fea0a7842d2f8b0fddb0f801fc91f33538627c
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-29 15:21:53 +0200

    MemoryBank.cs: Removed memory cache

--- AiCompanion/Memory/MemoryBank.cs
+++ AiCompanion/Memory/MemoryBank.cs
@@ -2,10 +2,5 @@
 
 internal class MemoryBank
 {
-    internal Dictionary<string, Memory> Memories = new();
-
-    internal IEnumerable<string> GetAllWords()
-    {
-        return Memories.Keys;
-    }
+    internal HashSet<string> Memories = new();
 }
